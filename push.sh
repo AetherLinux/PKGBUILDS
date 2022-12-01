faf0be377ede672c186885a508ecbb230ce74fb0
Dakkshesh <dakkshesh5@gmail.com>
2022-12-01 19:55:40 +0530

    [skip ci] push: Append .sig(s) to the array
    also solve some shellcheck warns.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- push.sh
+++ push.sh
@@ -3,19 +3,20 @@
 dir="$1"
 echo "$2"
 echo "dir: $dir"
-pkglist_name=$(echo $dir | tr '-' '_')
+pkglist_name=$(echo "$dir" | tr '-' '_')
 echo "pkglist_name: $pkglist_name"
 source "$2/pkglist"
 old_pkgs="$pkglist_name[@]"
-old_pkgs=(${!old_pkgs})
-echo "old: ${old_pkgs[@]}"
+old_pkgs=("${!old_pkgs}")
+echo "old: " "${old_pkgs[@]}"
 source "$2/../$dir/PKGBUILD"
 new_pkgs=("${pkgname[@]}")
 new_pkgs=("${new_pkgs[@]/%/-${pkgver}-${pkgrel}-${arch}.pkg.tar.zst}")
-echo "new: ${new_pkgs[@]}"
+echo "new:" "${new_pkgs[@]}"
 
 if [ -z "${old_pkgs[@]}" ]; then
-    echo -e "$pkglist_name=(${new_pkgs[@]})" >>"$2/pkglist"
+    new_pkgs_sigs=("${new_pkgs[@]/%/.sig}")
+    echo -e "$pkglist_name=(""${new_pkgs[@]}"" ${new_pkgs_sigs[@]}"")" >>"$2/pkglist"
 else
     for pkg in "${old_pkgs[@]}"; do
         echo "Removing: $pkg"
